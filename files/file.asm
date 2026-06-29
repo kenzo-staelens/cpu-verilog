4b30a180c9872d8b1d2fa2a391c1284068762772
Kenzo Staelens <kenzostael@gmail.be>
2026-06-29 16:54:52 +0200

    basic grammar plugin

--- files/file.asm
+++ files/file.asm
@@ -1,8 +1,11 @@
-add r1, r0, 0x3641
-add r2, r0, 0x3652
+add r1, zr, 0x3641
+add r2, zr, 0x3652
 str r1, 0x10
 str r2, 0x12
 add r4, r1, 1
 add r5, r2, 2
 add r3, r1, r2
 hlt
+.label test
+jeq.u test
+; test
